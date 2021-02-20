06c878c42025faf1c16cf31eff499fd7e11e4311
chenchaoqi <1023937946@qq.com>
2021-02-20 15:59:03 +0900

    刷题页面动画引导到做题页面

--- lib/jp_brush_questions/BrushQuestions.dart
+++ lib/jp_brush_questions/BrushQuestions.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:fsuper/fsuper.dart';
+import 'Today.dart';
 import 'bloc/CommonBloc.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:fsuper/fsuper.dart';
+import 'package:animations/animations.dart';
 
 class BrushQuestions extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var brightness =
         Provider.of<CommonBloc>(context, listen: true).themeData.brightness;
+    var N5Image = const AssetImage("images/kyotomomiji.jpg");
+    var N4Image = const AssetImage("images/donbori.jpg");
+    var N3Image = const AssetImage("images/fujisan.jpg");
+    var N2Image = const AssetImage("images/donbori.jpg");
+    var N1Image = const AssetImage("images/donbori.jpg");
     return CupertinoPageScaffold(
       child: CustomScrollView(
         slivers: [
@@ -25,11 +32,11 @@ class BrushQuestions extends StatelessWidget {
             sliver: SliverList(
               delegate: SliverChildListDelegate.fixed([
                 // 可滑动的 日语组件
-                JapaneseLevelScroller(context, "N5", "images/kyotomomiji.jpg"),
-                JapaneseLevelScroller(context, "N4", "images/donbori.jpg"),
-                JapaneseLevelScroller(context, "N3", "images/fujisan.jpg"),
-                JapaneseLevelScroller(context, "N2", "images/donbori.jpg"),
-                JapaneseLevelScroller(context, "N1", "images/donbori.jpg"),
+                JapaneseLevelScroller(context, "N5", N5Image,brightness),
+                JapaneseLevelScroller(context, "N4", N4Image,brightness),
+                JapaneseLevelScroller(context, "N3", N3Image,brightness),
+                JapaneseLevelScroller(context, "N2", N2Image,brightness),
+                JapaneseLevelScroller(context, "N1", N1Image,brightness),
 
                 // N5 框框
                 /*JapaneseLevel("N5", "images/kyotomomiji.jpg", () {
@@ -88,9 +95,18 @@ class BrushQuestions extends StatelessWidget {
 
   /// 日语考试等级 横向滑动
   Widget JapaneseLevelScroller(
-      BuildContext context, String level, String imageUrl) {
+      BuildContext context, String level, AssetImage levelImage, Brightness brightness) {
     // 文字颜色
     var wordColor = const Color(0xFF76D7C4);
+    ContainerTransitionType _transitionType = ContainerTransitionType.fade;
+
+    void _showMarkedAsDoneSnackbar(bool isMarkedAsDone) {
+      if (isMarkedAsDone ?? false)
+        Scaffold.of(context).showSnackBar(const SnackBar(
+          content: Text('Marked as done!'),
+        ));
+    }
+
     return Column(
       children: [
         Container(
@@ -103,15 +119,49 @@ class BrushQuestions extends StatelessWidget {
         ),
         // SizedBox(height: 16),
         Container(
-          height: 170,
+          height: 160,
           child: ListView(
             physics: BouncingScrollPhysics(),
             scrollDirection: Axis.horizontal,
             children: [
+              /// ios appstore 动画 弹出页面
+              OpenContainer<bool>(
+                closedShape: const RoundedRectangleBorder(),
+                closedElevation: 0.0,
+                closedColor:  brightness == Brightness.light
+                    ? CupertinoTheme.of(context).scaffoldBackgroundColor
+                    : CupertinoTheme.of(context).primaryContrastingColor,
+                transitionType: _transitionType,
+                openBuilder: (BuildContext context, VoidCallback _) {
+                  return  Today();
+                },
+                onClosed: _showMarkedAsDoneSnackbar,
+                tappable: true,
+                closedBuilder: (BuildContext _, VoidCallback openContainer) {
+                  return FSuper(
+                    text: AppLocalizations.of(context).comprehensive,
+                    style: TextStyle(
+                      fontSize: 65,
+                      fontWeight: FontWeight.bold,
+                      color: wordColor,
+                    ),
+                    textAlignment: Alignment.center,
+                    height: 160,
+                    margin: const EdgeInsets.fromLTRB(12.0, 10.0, 12.0, 10.0),
+                    padding: const EdgeInsets.fromLTRB(12.0, 10.0, 12.0, 10.0),
+                    width: MediaQuery.of(context).size.width * 0.65,
+                    corner: FCorner.all(15),
+                    shadowBlur: 5,
+                    backgroundImage: levelImage,
+                    onClick: () {
+                      openContainer();
+                    },
+                  );
+                },
+              ),
+
               /// 综合 就是模拟 考试了
-               FSuper(
-                isSupportNeumorphism: true,
-                float: true,
+              FSuper(
                 text: AppLocalizations.of(context).comprehensive,
                 style: TextStyle(
                   fontSize: 65,
@@ -120,19 +170,17 @@ class BrushQuestions extends StatelessWidget {
                 ),
                 textAlignment: Alignment.center,
                 height: 160,
-                margin: EdgeInsets.fromLTRB(12.0, 10.0, 12.0, 10.0),
-                padding: EdgeInsets.fromLTRB(12.0, 10.0, 12.0, 10.0),
+                margin: const EdgeInsets.fromLTRB(12.0, 10.0, 12.0, 10.0),
+                padding: const EdgeInsets.fromLTRB(12.0, 10.0, 12.0, 10.0),
                 width: MediaQuery.of(context).size.width * 0.65,
                 corner: FCorner.all(15),
                 shadowBlur: 5,
-                backgroundImage: AssetImage(imageUrl),
+                backgroundImage: levelImage,
                 onClick: () {},
               ),
 
               /// 文字词汇
               FSuper(
-                isSupportNeumorphism: true,
-                float: true,
                 text: AppLocalizations.of(context).words,
                 style: TextStyle(
                   fontSize: 65,
@@ -141,19 +189,17 @@ class BrushQuestions extends StatelessWidget {
                 ),
                 textAlignment: Alignment.center,
                 height: 160,
-                margin: EdgeInsets.fromLTRB(12.0, 10.0, 12.0, 10.0),
-                padding: EdgeInsets.fromLTRB(12.0, 10.0, 12.0, 10.0),
+                margin: const EdgeInsets.fromLTRB(12.0, 10.0, 12.0, 10.0),
+                padding: const EdgeInsets.fromLTRB(12.0, 10.0, 12.0, 10.0),
                 width: MediaQuery.of(context).size.width * 0.65,
                 corner: FCorner.all(15),
                 shadowBlur: 5,
-                backgroundImage: AssetImage(imageUrl),
+                backgroundImage: levelImage,
                 onClick: () {},
               ),
 
               /// 语法
               FSuper(
-                isSupportNeumorphism: true,
-                float: true,
                 text: AppLocalizations.of(context).grammar,
                 style: TextStyle(
                   fontSize: 65,
@@ -162,19 +208,17 @@ class BrushQuestions extends StatelessWidget {
                 ),
                 textAlignment: Alignment.center,
                 height: 160,
-                margin: EdgeInsets.fromLTRB(12.0, 10.0, 12.0, 10.0),
-                padding: EdgeInsets.fromLTRB(12.0, 10.0, 12.0, 10.0),
+                margin: const EdgeInsets.fromLTRB(12.0, 10.0, 12.0, 10.0),
+                padding: const EdgeInsets.fromLTRB(12.0, 10.0, 12.0, 10.0),
                 width: MediaQuery.of(context).size.width * 0.65,
                 corner: FCorner.all(15),
                 shadowBlur: 5,
-                backgroundImage: AssetImage(imageUrl),
+                backgroundImage: levelImage,
                 onClick: () {},
               ),
 
               /// 阅读理解
               FSuper(
-                isSupportNeumorphism: true,
-                float: true,
                 text: AppLocalizations.of(context).read,
                 style: TextStyle(
                   fontSize: 65,
@@ -183,19 +227,17 @@ class BrushQuestions extends StatelessWidget {
                 ),
                 textAlignment: Alignment.center,
                 height: 160,
-                margin: EdgeInsets.fromLTRB(12.0, 10.0, 12.0, 10.0),
-                padding: EdgeInsets.fromLTRB(12.0, 10.0, 12.0, 10.0),
+                margin: const EdgeInsets.fromLTRB(12.0, 10.0, 12.0, 10.0),
+                padding: const EdgeInsets.fromLTRB(12.0, 10.0, 12.0, 10.0),
                 width: MediaQuery.of(context).size.width * 0.65,
                 corner: FCorner.all(15),
                 shadowBlur: 5,
-                backgroundImage: AssetImage(imageUrl),
+                backgroundImage: levelImage,
                 onClick: () {},
               ),
 
               /// 听力
               FSuper(
-                isSupportNeumorphism: true,
-                float: true,
                 text: AppLocalizations.of(context).listen,
                 style: TextStyle(
                   fontSize: 65,
@@ -204,12 +246,12 @@ class BrushQuestions extends StatelessWidget {
                 ),
                 textAlignment: Alignment.center,
                 height: 160,
-                margin: EdgeInsets.fromLTRB(12.0, 10.0, 12.0, 10.0),
-                padding: EdgeInsets.fromLTRB(12.0, 10.0, 12.0, 10.0),
+                margin: const EdgeInsets.fromLTRB(12.0, 10.0, 12.0, 10.0),
+                padding: const EdgeInsets.fromLTRB(12.0, 10.0, 12.0, 10.0),
                 width: MediaQuery.of(context).size.width * 0.65,
                 corner: FCorner.all(15),
                 shadowBlur: 5,
-                backgroundImage: AssetImage(imageUrl),
+                backgroundImage: levelImage,
                 onClick: () {},
               ),
             ],
